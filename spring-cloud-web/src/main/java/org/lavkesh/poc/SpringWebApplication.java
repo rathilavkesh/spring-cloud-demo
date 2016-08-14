aeba58fc796f67373ab010563653a116f4b75aaa
Lavkesh Rathi <lavkeshrathi2006@gmail.com>
2016-08-13 23:17:40 -0400

    Adding netflex zuul

--- spring-cloud-web/src/main/java/org/lavkesh/poc/SpringWebApplication.java
+++ spring-cloud-web/src/main/java/org/lavkesh/poc/SpringWebApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableHystrix
+@EnableZuulProxy
 public class SpringWebApplication {
 
 	public static void main(String[] args) throws Exception {
